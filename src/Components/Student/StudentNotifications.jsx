88412dbfb93bf4d76bbed9c34ad18d3ca5f77ec3
Majdag8 <majdag2005@gmail.com>
2025-02-14 19:36:40 +0200

    updated

--- src/Components/Student/StudentNotifications.jsx
+++ src/Components/Student/StudentNotifications.jsx
@@ -28,7 +28,74 @@ export default function StudentNotifications(){
     
             </div>
             <div className="gray-line"></div>
+            <div className="notifications">
+                <div style={{
+                    display:"flex",
+                    flexDirection:"column",
+                    gap:"5px"
+                }}>
+                <div className="notification-title-container">
+                    <img className="purple-circle" src="../public/Images/Purple-circle.png" alt="" />
+                    <p className="notification-title">Absence Warning</p>
+                </div>
 
+                <div className="temp">
+                    <div className="notification-content-container">
+                        <p className="notification-content">You missed your Introduction to</p>
+                        <p className="course-name">Management Course</p>
+                    </div>  
+
+                </div>
+                </div>
+                <button className="mark-as-read">Mark As Read</button>
+    
+            </div>
+            <div className="gray-line"></div>
+            <div className="notifications">
+                <div style={{
+                    display:"flex",
+                    flexDirection:"column",
+                    gap:"5px"
+                }}>
+                <div className="notification-title-container">
+                    <img className="purple-circle" src="../public/Images/Purple-circle.png" alt="" />
+                    <p className="notification-title">Absence Warning</p>
+                </div>
+
+                <div className="temp">
+                    <div className="notification-content-container">
+                        <p className="notification-content">You missed your Introduction to</p>
+                        <p className="course-name">Management Course</p>
+                    </div>  
+
+                </div>
+                </div>
+                <button className="mark-as-read">Mark As Read</button>
+    
+            </div>
+            <div className="gray-line"></div>
+            <div className="notifications">
+                <div style={{
+                    display:"flex",
+                    flexDirection:"column",
+                    gap:"5px"
+                }}>
+                <div className="notification-title-container">
+                    <img className="purple-circle" src="../public/Images/Purple-circle.png" alt="" />
+                    <p className="notification-title">Absence Warning</p>
+                </div>
+
+                <div className="temp">
+                    <div className="notification-content-container">
+                        <p className="notification-content">You missed your Introduction to</p>
+                        <p className="course-name">Management Course</p>
+                    </div>  
+
+                </div>
+                </div>
+                <button className="mark-as-read">Mark As Read</button>
+    
+            </div>
         </div>
     )   
 }
